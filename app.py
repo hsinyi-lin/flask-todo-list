36f08d761159d731706b6944e33af961cc4a3079
Hsin Yi Lin <10656025@ntub.edu.tw>
2022-05-25 19:38:54 +0800

    feat: add update route

--- app.py
+++ app.py
@@ -29,6 +29,13 @@ def add():
     return redirect(url_for('index'))
 
 
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
 if __name__ == "__main__":
     db.create_all()
 
