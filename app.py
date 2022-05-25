ac3e20630a1a654bcbfe9c09c253e0e92ff90aee
Hsin Yi Lin <10656025@ntub.edu.tw>
2022-05-25 19:39:06 +0800

    feat: add delete route

--- app.py
+++ app.py
@@ -36,6 +36,15 @@ def update(todo_id):
     db.session.commit()
     return redirect(url_for('index'))
 
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
 if __name__ == "__main__":
     db.create_all()
 
